00045bc80049208f6edd73015512958f84db152c
Alex <alexandr@hobby4me.ru>
2017-09-26 15:05:46 +0300

    Remove 'full_slug' cache field

--- clean_urls/models.py
+++ clean_urls/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 
 class CleanURLMixin(models.Model):
-    MAX_LENGTH = 255
-
-    full_slug = models.CharField(editable=False, max_length=MAX_LENGTH, unique=True)
-
     class Meta:
         abstract = True
 
@@ -14,13 +10,9 @@ class CleanURLMixin(models.Model):
 
     def get_parents(self):  # includes self
         parent = self.get_parent()
-        return (parent.get_parents() if parent else []) + [self]
+        return (list(parent.get_parents()) if parent else []) + [self]
 
-    def get_full_slug(self, *args, **kwargs):
-        return '/'.join([parent.slug for parent in self.get_parents()])[:self.MAX_LENGTH-1] + '/'
-
-    def save(self, *args, **kwargs):
-        if not self.full_slug:
-            self.full_slug = self.get_full_slug()
-        return super().save(*args, **kwargs)
-
+    # TODO: cache this
+    @property
+    def full_slug(self, *args, **kwargs):
+        return '/'.join([parent.slug for parent in self.get_parents()]) + '/'
--- clean_urls/views.py
+++ clean_urls/views.py
@@ -15,11 +15,15 @@ class CleanURLHandler:
     def __call__(self, *args, **kwargs):
         slug = kwargs['slug']
 
+        last_slug = slug.split('/')[-2]
         for queryset, view in self.settings:
-            try:
-                instance = queryset.get(full_slug=slug)
-                return view(*args, **{**kwargs, **{'instance': instance}})
-            except ObjectDoesNotExist:
-                pass
+            matches = [instance for instance in queryset.filter(slug=last_slug) if instance.full_slug == slug]
+            if len(matches) > 1:
+                raise queryset.model.MultipleObjectsReturned('Slug "{slug}" has multiple matches: {matches}'.format(
+                    slug=slug,
+                    matches='; '.join([str(match) for match in matches]),
+                ))
+            elif len(matches) == 1:
+                return view(*args, **{**kwargs, **{'instance': matches[0]}})
 
         raise Http404()
